52b3157111fde13bdd2ca0eb202df399afde75c8
AylenCenturion <104035864+AylenCenturion@users.noreply.github.com>
2022-10-14 21:07:53 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const cartMenu = document.querySelector(".cart");
+const cartBtn = document.querySelector(".cart-div");
+const backBlur = document.querySelector(".blur");
+
+const toggleCart = () => {
+  cartMenu.classList.toggle("open-cart");
+  backBlur.classList.toggle("see-blur");
+};
+
+const closeCart = (e) => {
+  if (!e.target.classList.contains("close-cart")) return;
+  cartMenu.classList.remove("open-cart");
+  backBlur.classList.remove("see-blur");
+}
+
+const closeOnScroll = () => {
+  if(!cartMenu.classList.contains("open-cart")) return;
+  cartMenu.classList.remove("open-cart");
+  backBlur.classList.remove("see-blur");
+}
+
+//Falta logica de carrito
+
+
+const init = () => {
+  cartBtn.addEventListener("click", toggleCart);
+  cartMenu.addEventListener("click", closeCart);
+  window.addEventListener("scroll, closeOnScroll")
+
+};
+
+init()

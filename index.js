792f64b8089b0425e6920d6642ae01a4b742c4f2
Francisco Salgado <Frannsalgado8@gmail.com>
2022-10-24 13:18:36 -0300

    open-close cart js (display)

--- index.js
+++ index.js
@@ -3,18 +3,21 @@ const cartBtn = document.querySelector(".cart-div");
 const backBlur = document.querySelector(".blur");
 
 const toggleCart = () => {
+  cartMenu.style.display = "flex"
   cartMenu.classList.toggle("open-cart");
   backBlur.classList.toggle("see-blur");
 };
 
 const closeCart = (e) => {
   if (!e.target.classList.contains("close-cart")) return;
+  cartMenu.style.display = "none"
+
   cartMenu.classList.remove("open-cart");
   backBlur.classList.remove("see-blur");
 }
 
 const closeOnScroll = () => {
-  if(!cartMenu.classList.contains("open-cart")) return;
+  if (!cartMenu.classList.contains("open-cart")) return;
   cartMenu.classList.remove("open-cart");
   backBlur.classList.remove("see-blur");
 }

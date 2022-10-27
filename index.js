7028b9e459bab27f3e034cea71b36de70fd5c915
Francisco Salgado <Frannsalgado8@gmail.com>
2022-10-27 19:28:26 -0300

    Datos, responsive

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+import { productos } from "./productos.js";
 const cartMenu = document.querySelector(".cart");
 const cartBtn = document.querySelector(".cart-div");
 const backBlur = document.querySelector(".blur");
+
+//categorias --
+const popularesBtn = document.getElementById("populares");
+const pizzasBtn = document.getElementById("pizzas");
+const burgerBtn = document.getElementById("burgers");
+const papasBtn = document.getElementById("papas");
+const wrapsBtn = document.getElementById("wraps");
+const MxFoodBtn = document.getElementById("MxFood");
+const batidosBtn = document.getElementById("batidos");
+//categorias fin
+
+const productoContainer = document.querySelector(".popular-products-container");
 
 const toggleCart = () => {
   cartMenu.style.display = "flex";
@@ -22,12 +35,60 @@ const closeOnScroll = () => {
   backBlur.classList.remove("see-blur");
 };
 
-//Falta logica de carrito
+//Falta logica de carrito//
+
+//Renderizar Productos--
+const renderProductos = () => {
+
+  whoActive()
+
+
+}
+
+// const whoActive = () => {
+//   if (popularesBtn.classList.contains("active")) {
+//     return (console.log("sisi"))
+//   }
+//   if (pizzasBtn.classList.contains("active")) {
+//     return (console.log("sisi"))
+//   }
+//   if (burgerBtn.classList.contains("active")) {
+//     return (console.log("sisi"))
+//   }
+//   if (papasBtn.classList.contains("active")) {
+//     return (console.log("sisi"))
+//   }
+//   if (wrapsBtn.classList.contains("active")) {
+//     return (console.log("sisi"))
+//   }
+//   if (MxFoodBtn.classList.contains("active")) {
+//     return (console.log("sisi"))
+//   }
+//   if (batidosBtn.classList.contains("active")) {
+//     return (console.log("sisi"))
+//   }
+
+//   else {
+//     return (console.log("ERROR"))
+//   }
+// }
+
+
+
 
 const init = () => {
   cartBtn.addEventListener("click", toggleCart);
   cartMenu.addEventListener("click", closeCart);
   window.addEventListener("scroll", closeOnScroll);
+  pizzasBtn.addEventListener("click", renderProductos);
+  popularesBtn.addEventListener("click", renderProductos);
+  burgerBtn.addEventListener("click", renderProductos);
+  papasBtn.addEventListener("click", renderProductos);
+  wrapsBtn.addEventListener("click", renderProductos);
+  MxFoodBtn.addEventListener("click", renderProductos);
+  batidosBtn.addEventListener("click", renderProductos);
+
+  console.log(productos)
 };
 
 init();
---
+++ productos.js
@@ -0,0 +1,287 @@
+
+const pizzas =
+    [
+        {
+            id: 0,
+            nombre: "La Mr.Pit",
+            descripcion: "Solo para Expertos",
+            img: "Assets/Mr.pit.png",
+            precio: 350,
+            popularidad: true,
+
+        },
+        {
+            id: 1,
+            nombre: "¡Q'Jamone!",
+            descripcion: "c/jamón crudo",
+            img: "Assets/Q,jamone.png",
+            precio: 400,
+            popularidad: true,
+
+        },
+        {
+            id: 2,
+            nombre: "La Charly García",
+            descripcion: "¡BASTA!",
+            img: "Assets/CharliG.png",
+            precio: 500,
+            popularidad: true,
+
+
+        },
+        {
+            id: 3,
+            nombre: "La Maradona",
+            descripcion: "¡Eterna!",
+            img: "Assets/Maradona.png",
+            precio: 650,
+            popularidad: true,
+
+
+        },
+        {
+            id: 4,
+            nombre: "Picantovich",
+            descripcion: "Pica dos veces",
+            img: "Assets/picantovich.png",
+            precio: 700,
+            popularidad: true,
+
+
+        },
+        {
+            id: 5,
+            nombre: "La Hasbulla",
+            descripcion: "En honor al 1",
+            img: "Assets/Hasbulla.png",
+            precio: 480,
+            popularidad: true,
+
+
+        },
+        {
+            id: 6,
+            nombre: "Leo Messi",
+            descripcion: "¡De pie señores!",
+            img: "Assets/LeoMessi.png",
+            precio: 570,
+            popularidad: true,
+
+
+        },
+        {
+            id: 7,
+            nombre: "La Nick Gi",
+            descripcion: "Solo para expertos",
+            img: "Assets/NickG.png",
+            precio: 620,
+            popularidad: true,
+
+
+        },
+        {
+            id: 8,
+            nombre: "Bennazianna",
+            descripcion: "La más completa",
+            img: "Assets/bennazianna.png",
+            precio: 870,
+            popularidad: false,
+
+
+        },
+        {
+            id: 9,
+            nombre: "Tronco-Pizza",
+            descripcion: "Para todo el día",
+            img: "Assets/troncoPizza.png",
+            precio: 940,
+            popularidad: false,
+
+
+        },
+
+
+    ]
+
+
+const hamburgesas = [
+    {
+        id: 0,
+        nombre: "Clasica",
+        descripcion: "Completa",
+        img: "Assets/burgers/Bclasica.png",
+        precio: 350,
+        popularidad: false,
+
+    },
+    {
+        id: 1,
+        nombre: "Doble Beacon",
+        descripcion: "Beacon Crocante",
+        img: "Assets/burgers/Bbeacon.png",
+        precio: 850,
+        popularidad: true,
+
+    },
+    {
+        id: 2,
+        nombre: "Doble",
+        descripcion: "Mas Grande",
+        img: "Assets/burgers/Bdoble.png",
+        precio: 550,
+        popularidad: false,
+
+    },
+    {
+        id: 3,
+        nombre: "Fundida",
+        descripcion: "Queso Fundido",
+        img: "Assets/burgers/Bfundida.png",
+        precio: 650,
+        popularidad: false,
+
+    },
+    {
+        id: 4,
+        nombre: "Oscura",
+        descripcion: "¡?EXTRAVAGANTE!",
+        img: "Assets/burgers/Bnegra.png",
+        precio: 850,
+        popularidad: true,
+
+    },
+
+]
+const papas = [
+    {
+        id: 0,
+        nombre: "Papas Bravas",
+        descripcion: "Clasico",
+        img: "Assets/papas/pBravas.png",
+        precio: 350,
+        popularidad: false,
+
+    },
+    {
+        id: 1,
+        nombre: "Papas Provenzal",
+        descripcion: "Clasicos",
+        img: "Assets/papas/provenzal.png",
+        precio: 450,
+        popularidad: true,
+
+    },
+
+    {
+        id: 0,
+        nombre: "Papas Rusticas",
+        descripcion: "Clasicos",
+        img: "Assets/papas/pRusticas",
+        precio: 550,
+        popularidad: false,
+
+    },
+
+]
+
+const wraps = [
+    {
+        id: 0,
+        nombre: "Carne",
+        descripcion: "Clasico",
+        img: "Assets/wraps/Wcarne.jpg",
+        precio: 350,
+        popularidad: true,
+
+    },
+    {
+        id: 1,
+        nombre: "Picante",
+        descripcion: "Duele",
+        img: "Assets/wraps/Wpicante.jpg",
+        precio: 550,
+        popularidad: false,
+
+    },
+    {
+        id: 2,
+        nombre: "Pollo",
+        descripcion: "Pollito",
+        img: "Assets/wraps/Wpollo.jpg",
+        precio: 670,
+        popularidad: false,
+
+    },
+    {
+        id: 3,
+        nombre: "Vegi",
+        descripcion: "Yuyos",
+        img: "Assets/wraps/Wvegetales.jpg",
+        precio: 450,
+        popularidad: false,
+
+    },
+
+]
+const mexicanFood = [
+    {
+        id: 0,
+        nombre: "Carnitos",
+        descripcion: "Clasico",
+        img: "Assets/MX/Tcarne.png",
+        precio: 350,
+        popularidad: true,
+
+    },
+    {
+        id: 1,
+        nombre: "Don Camaron",
+        descripcion: "En Don Camaron...",
+        img: "Assets/MX/Tcamaron.png",
+        precio: 350,
+        popularidad: false,
+
+    },
+    {
+        id: 2,
+        nombre: "Al Pastor",
+        descripcion: "YESS!!",
+        img: "Assets/MX/Tpastor.png",
+        precio: 350,
+        popularidad: false,
+
+    },
+
+]
+
+const milShakes = [
+    {
+        id: 0,
+        nombre: "Chocolate",
+        descripcion: "Explosion de Sabor",
+        img: "Assets/MilkS/MSchocolate.png",
+        precio: 350,
+        popularidad: true,
+
+    },
+    {
+        id: 0,
+        nombre: "Cooki",
+        descripcion: "Clasico",
+        img: "Assets/MilkS/MScockies.png",
+        precio: 350,
+        popularidad: false,
+
+    }, {
+        id: 0,
+        nombre: "Oreo",
+        descripcion: "",
+        img: "Assets/MilkS/MSoreo.png",
+        precio: 350,
+        popularidad: false,
+
+    },
+]
+
+
+export const productos = [pizzas, hamburgesas, papas, wraps, mexicanFood, milShakes]
